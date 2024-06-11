abfd06900308e8287387356e9f35dec0bf072cef
usertam <heiyiutam@gmail.com>
2024-06-12 01:44:09 +0800

    flake.nix: install final pdf only; create build stages

--- flake.nix
+++ flake.nix
@@ -14,23 +14,27 @@
       default = pkgs.stdenvNoCC.mkDerivation (final: {
         name = "Curriculum Vitae";
         src = self;
+
         nativeBuildInputs = with pkgs; [
           typst
           ghostscript
           exiftool
           qpdf
         ] ++ builtins.attrValues fonts;
+
         enableParallelBuilding = true;
+
         TYPST_FONT_PATHS = builtins.concatStringsSep ":" (map
+          # Force truetype fonts for now
           (f: f + "/share/fonts/truetype")
           (builtins.attrValues fonts));
+
         buildPhase = ''
-          mkdir -p $out
-          typst compile $src/main.typ \
-            "$out/${final.name}.pdf"
-        '';
-        installPhase = ''
-          gs -o a.pdf \
+          echo "Build stage 1: compile typst source"
+          typst compile $src/main.typ build-stage-1.pdf
+
+          echo "Build stage 2: convert to PDF/A-2b with ghostscript"
+          gs -o build-stage-2.pdf \
             -dBATCH -dNOPAUSE -dNOOUTERSAVE \
             -dPDFA=2 \
             -sColorConversionStrategy=UseDeviceIndependentColor \
@@ -41,10 +45,18 @@
             -sInstanceUUID=00000000-0000-0000-0000-000000000000 \
             -dOmitInfoDate=true -dOmitID=true \
             -dNumRenderingThreads=$NIX_BUILD_CORES \
-            -f "$out/${final.name}.pdf"
-          exiftool -producer= -documentid= -overwrite_original a.pdf
-          qpdf --deterministic-id --linearize --newline-before-endstream \
-            a.pdf "$out/${final.name} (A-2b).pdf"
+            -f build-stage-1.pdf
+
+          echo "Build stage 3: remove specific metadata with exiftool"
+          exiftool -producer= -documentid= -verbose build-stage-2.pdf -o build-stage-3.pdf
+
+          echo "Build stage 4: linearize with qpdf"
+          qpdf --deterministic-id --linearize --newline-before-endstream --verbose \
+            build-stage-3.pdf build-stage-4.pdf
+        '';
+
+        installPhase = ''
+          install -Dm444 build-stage-4.pdf "$out/${final.name}.pdf"
         '';
       });
     });
